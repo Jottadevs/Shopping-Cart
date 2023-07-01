2496cd8712e988c9b76cf71227c7698898fce0a6
Jottadevs <joaojlleadro4@gmail.com>
2023-06-30 22:22:39 -0300

    searchbar-code

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,33 @@
+import { BsSearch } from 'react-icons/bs'
+import { useState } from 'react';
+
+import './SearchBar.css'
+
+
+function SearchBar() {
+
+    const [searchValue, setSearchValue] = useState('')
+
+    return (
+        <form className='search-bar'>
+            <input
+                type="search"
+                value={searchValue}
+                placeholder="Buscar Produtos"
+                className="search__input"
+                onChange={({ target }) => setSearchValue(target.value)}
+                required
+            />
+
+            {searchValue}
+
+            <button
+                type="submit"
+                className="search__button">
+                <BsSearch />
+            </button>
+        </form>
+    );
+}
+
+export default SearchBar;
