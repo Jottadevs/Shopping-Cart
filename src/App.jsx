1838a8a73d65740f8c0c74ecdef1bdeb9c993d8d
Jottadevs <joaojlleadro4@gmail.com>
2023-07-01 16:08:54 -0300

    app-code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header"
+import Products from "./components/Products/Products"
 
 function App() {
 
   return (
     <div>
       <Header />
+      <Products />
     </div>
   )
 }
